b8ff391beeb24fea40b4fad27ad811706a321b60
juimin <d95wang@gmail.com>
2021-11-21 22:57:40 -0800

    Delete stuff we don't really need

--- background.js
+++
@@ -1,6 +0,0 @@
-const EXTENSION_NAME = "comment_eraser";
-
-// Event listener
-chrome.runtime.onInstalled.addListener(() => {
-    console.log(`${EXTENSION_NAME} installed`)
-});
--- content_scripts/youtube/override.js
+++
@@ -1,2 +0,0 @@
-// Eliminate the comments section from youtube
-console.log("Hiding youtube comments.");
